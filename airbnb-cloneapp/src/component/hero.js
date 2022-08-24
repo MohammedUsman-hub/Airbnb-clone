149c43b8c68f91608269b897311c506c839b7fcb
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-08-24 01:55:19 +0100

    update 2

--- airbnb-cloneapp/src/component/hero.js
+++ airbnb-cloneapp/src/component/hero.js
@@ -7,8 +7,8 @@ export default class GridImg extends Component{
         return(
             <section className='hero'>
                 <img id='grid-img' src={ gridImg }></img>
-                <h2 className='online-ex'>Online Experiences</h2>
-                <p>Join unique interactive activies led by one-of-a-kind hosts-all without leaving home.</p>
+                <h2 className='hero-h2'>Online Experiences</h2>
+                <p className='hero-p'>Join unique interactive activies led by one-of-a-kind hosts-all without leaving home.</p>
             </section>
         )
     }
