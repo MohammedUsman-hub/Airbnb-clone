f529841bec0fea9fc12c2a55956ff18a7ef3dc0a
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-08-28 01:39:44 +0100

    update 3

--- airbnb-cloneapp/src/App.js
+++ airbnb-cloneapp/src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Nav from './component/navbar'
-import GridImg from "./component/hero"
+import Hero from "./component/hero"
+
 
 export default function App(){
   return(
     <div>
       <Nav/>
-      <GridImg/>
+      <Hero/>
+     
+
+      
     </div>
   )
 }
