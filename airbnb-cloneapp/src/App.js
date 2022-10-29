71a788f652a2ff7587cd206e2749b37a0432ae56
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2022-10-29 20:30:27 +0100

    PitStop

--- airbnb-cloneapp/src/App.js
+++ airbnb-cloneapp/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import Nav from './component/navbar'
 import Hero from "./component/hero"
 import Card from "./component/card"
+import Contact from "./contact"
 
 
 export default function App(){
@@ -11,6 +12,32 @@ export default function App(){
       <Hero/>
       <Card/>
 
+
+      <Contact
+        img="./images/cat.jpg"
+        name="Cookie"
+        phone="07625188827"
+        mail="cookie@gmail.com"
+      />
+      <Contact
+        img="./images/cat3.jpg"
+        name="Kelly"
+        phone="07627865933"
+        mail="Kelly@gmail.com"
+      />
+      <Contact
+        img="./images/cat3.jpg"
+        name="Billy"
+        phone="07002749992"
+        mail="billy@gmail.com"
+      />
+      <Contact
+        img="./images/cat4.jpg"
+        name="Krissy"
+        phone="07142356744"
+        mail="Krissy@gmail.com"
+      />
+     
       
     </div>
   )
---
+++ airbnb-cloneapp/src/contact.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./index.css"
+
+import Phone from "./images/phone.png"
+import Cat from "./images/cat.jpg"
+import Mail from "./images/mail.jpg"
+
+
+export default function Contact(props){
+        return(
+            <div className='contacts'>
+
+                <div className='contact-card'>
+                    <img src={props.img}/>
+                    <h3>{props.name}</h3>
+
+                    <div className='info-group'>
+                        <img src={Phone}></img>
+                        <p>{props.phone}</p>
+                    </div>
+
+                    <div className='info-group'>
+                        <img src={Mail}></img>
+                        <p>{props.mail}</p>
+                    </div>
+
+                </div>
+                
+            </div>
+        )
+    
+}
