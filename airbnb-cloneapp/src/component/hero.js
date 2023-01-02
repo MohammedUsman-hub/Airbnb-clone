30f4e971ddfca6a21070fbf842ea1d74cd2287e9
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-02 11:36:10 +0000

    adding to data.js

--- airbnb-cloneapp/src/component/hero.js
+++ airbnb-cloneapp/src/component/hero.js
@@ -5,7 +5,10 @@ export default class GridImg extends Component{
     render(){
         return(
             <section className='hero'>
-                <img id='grid-img' src="images/grid-img.png"></img>
+             {/*   <img id='grid-img' src="images/grid-img.png"></img>*/}
+
+
+                
                 <h2 className='hero-h2'>Online Experiences</h2>
                 <p className='hero-p'>Join unique interactive activies led by one-of-a-kind hosts-all without leaving home.</p>
             </section>
--- airbnb-cloneapp/src/data.js
+++ airbnb-cloneapp/src/data.js
@@ -3,7 +3,7 @@ export default [
         id: 1,
         title: "Life Lessons with Michael Jordan",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-        price: 136,
+        price: 1000,
         img: "mj.jpg",
         stats: {
             rating: 5.0,
@@ -38,6 +38,71 @@ export default [
         location: "Norway",
         openSpots: 3,
     },
+    {
+        id: 4,
+        title: "Cooking Korean food with Kpop idol Yuri of Girls Generation",
+        description: "Experience cooking all sorts of Korean recipes with Girls Generations very own Yuri.",
+        price: 150,
+        img: "yuri.jpg",
+        stats: {
+            rating: 5,
+            reviewCount: 69
+        },
+        location: "Korea",
+        openSpots: 19,
+    },
+    {
+        id: 5,
+        title: "Plan your trip to Paris with Anna",
+        description: "Join Anna as she will guide you to planning the best trip to Paris within your budget.",
+        price: 40,
+        img: "paris.jpg",
+        stats: {
+            rating: 3.9,
+            reviewCount:9 
+        },
+        location: "Online",
+        openSpots: 3,
+    },
+    {
+        id: 6,
+        title: "Music production class with Jmin",
+        description: "Upcomming rapper Jmin will be happy to teach you how to produce music.",
+        price: 60,
+        img: "jmin.jpg",
+        stats: {
+            rating: 5,
+            reviewCount: 99 
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 7,
+        title: "The basics of art with Jane",
+        description: "Jane has 10 year experince in art, she is willing to show off her skills and teach others the basic.",
+        price: 20,
+        img: "artTeacher.jpg",
+        stats: {
+            rating: 2.5,
+            reviewCount: 22
+        },
+        location: "London",
+        openSpots: 1,
+    },
+    {
+        id: 8,
+        title: "Make mexican street tacos with a pro chef",
+        description: "make tacos",
+        price: 12,
+        img: "chef.jpg",
+        stats: {
+            rating: 5,
+            reviewCount: 55
+        },
+        location: "Online",
+        openSpots: 1,
+    }
   
     
 ]
