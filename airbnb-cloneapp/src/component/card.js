9bb0c36fb12d6e4fb580d5204a773531c6cef288
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-01-02 09:25:54 +0000

    first commit of 2023

--- airbnb-cloneapp/src/component/card.js
+++ airbnb-cloneapp/src/component/card.js
@@ -2,15 +2,19 @@ import React from 'react'
 import "../index.css"
 
 export default function Card(props) {
+  
+  
   let badgeText
   if (props.openSports === 0) {
     badgeText = "SOLD OUT"
   }else if (props.location === "Online"){
     badgeText = "ONLINE"
-  }
+  } 
+
+
         return(
             <div className='card'>
-             {props.openSports === 0 && <div className='card--badge'>SOLD OUT</div>}
+             {badgeText && <div className='card--badge'>{badgeText}</div>}
                 <img class="card--image" src={`../images/${props.img}`}></img>  
                   <div className='card-stats'>
                     <img class="card--star" src={process.env.PUBLIC_URL + '/images/star.png'}/>
